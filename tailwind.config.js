74ab0d3d7337a241ac844d5ab289f87a6c290ac5
Alfagov <lorenzopulcini@protonmail.ch>
2023-08-31 17:22:21 +0200

    "Update grid page with new features and designs"
    The gridPage.html template has been expanded and restructured. New features, including new SVG icons, tags and templates have been added. This increases the possibilities for customization and enhances the overall user experience. The page structure has been optimized for better responsiveness. New CSS classes were added for improved layout visuals in dark mode and additional metrics information has also been added to the template. Flexibility was enhanced by removing hard-coded sections and substituting them with block sections. This change was necessary to make the layout more adaptive and maintainable.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 const plugin = require('tailwindcss/plugin')
 module.exports = {
-  darkMode: 'media',
-  content: [
-      "./templates/*.html",
-      "./node_modules/flowbite/**/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-      require("flowbite/plugin"),
-      plugin(function({ addVariant }) {
-          addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &'])
-          addVariant('htmx-request',  ['&.htmx-request',  '.htmx-request &'])
-          addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &'])
-          addVariant('htmx-added',    ['&.htmx-added',    '.htmx-added &'])
-      }),
-  ],
+    darkMode: 'media',
+    content: [
+        "./templates/*.html",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("flowbite/plugin"),
+        plugin(function ({addVariant}) {
+            addVariant('htmx-settling', ['&.htmx-settling', '.htmx-settling &'])
+            addVariant('htmx-request', ['&.htmx-request', '.htmx-request &'])
+            addVariant('htmx-swapping', ['&.htmx-swapping', '.htmx-swapping &'])
+            addVariant('htmx-added', ['&.htmx-added', '.htmx-added &'])
+        }),
+        plugin(function ({addUtilities}) {
+            addUtilities({
+                /* Hide scrollbar for Chrome, Safari and Opera */
+                '.no-scrollbar::-webkit-scrollbar': {
+                    'display': 'none'
+                },
+
+                /* Hide scrollbar for IE, Edge and Firefox */
+                '.no-scrollbar': {
+                    '-ms-overflow-style': 'none', /* IE and Edge */
+                    'scrollbar-width': 'none'  /* Firefox */
+                }
+            })
+        })
+    ],
 }
 
